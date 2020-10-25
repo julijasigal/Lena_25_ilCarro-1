69a26f645cd16998a2dbac262f348dca9de821d8
Lena <etarnovskaya@gmail.com>
2020-10-25 21:03:44 +0200

    added Logger

--- src/test/java/com/ilcarro/qa/tests/TestBase.java
+++ src/test/java/com/ilcarro/qa/tests/TestBase.java
@@ -2,23 +2,37 @@ package com.ilcarro.qa.tests;
 
 import com.ilcarro.qa.framework.ApplicationManager;
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.*;
+
+import java.lang.reflect.Method;
 
 public class TestBase {
 
     protected static ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
 
     @BeforeSuite
-    public void setUp(){
+    public void setUp() {
         app.init();
         // login("aa@bb109.com", "1Aaaaaaaa");
+    }
 
+    @BeforeMethod
+    public void startTest(Method m) {
+        logger.info("Start test " + m.getName());
+    }
+
+    @AfterMethod
+    public void stopTest(Method m) {
+        logger.info("Stop test " + m.getName());
+        logger.info("======================================================================");
     }
 
 
-
     @AfterSuite(enabled = true)
-    public void tearDown(){
+    public void tearDown() {
         app.stop();
     }
 
